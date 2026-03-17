1f7b83d1bc91de7b8ca25a7ecc5f9929b99e226c
tenderhusky <vivekkumar@tenderhusky.com>
2026-03-17 20:23:57 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // This ensures your JSON data is accessible during the build
+  serverExternalPackages: [], 
+};
+
+export default nextConfig;
